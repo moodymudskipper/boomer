16db5a66a068ea1ebcbc299a3e4f791d0c5f8cfa
Antoine Fabri <antoine.fabri@gmail.com>
2021-06-22 12:59:09 +0200

    Remove obsolete bquote2()

--- tests/testthat/test-bquote2.R
+++
@@ -1,8 +0,0 @@
-test_that("bquote2() works", {
-  expect_identical(
-    bquote2(.IF(TRUE, a)), quote(a))
-  expect_identical(
-    bquote2({.IF(FALSE, a)}), quote({}))
-  expect_identical(
-    bquote2({.IF(FALSE, a, b)}), quote({b}))
-})
