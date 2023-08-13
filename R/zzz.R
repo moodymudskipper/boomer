b18147928e9925d01e72c2f2d098db00a814b09b
Kirill Müller <kirill@cynkra.com>
2023-08-13 11:50:38 +0200

    Stabilize tests

--- R/zzz.R
+++ R/zzz.R
@@ -4,18 +4,19 @@
 #' @importFrom pryr address
 NULL
 
+op.boom <- list(
+  boomer.clock = FALSE,
+  boomer.print = print,
+  boomer.ignore = c("~", "{", "(", "<-", "<<-", "="),
+  boomer.visible_only = FALSE,
+  boomer.print_args = TRUE,
+  boomer.safe_print = FALSE,
+  boomer.abbreviate = FALSE
+)
+
 .onLoad <- function(libname, pkgname) {
   #nocov start
   op <- options()
-  op.boom <- list(
-    boomer.clock = FALSE,
-    boomer.print = print,
-    boomer.ignore = c("~", "{", "(", "<-", "<<-", "="),
-    boomer.visible_only = FALSE,
-    boomer.print_args = TRUE,
-    boomer.safe_print = FALSE,
-    boomer.abbreviate = FALSE
-  )
   toset <- !(names(op.boom) %in% names(op))
   if(any(toset)) options(op.boom[toset])
   #nocov end
---
+++ tests/testthat/setup.R
@@ -0,0 +1,1 @@
+withr::local_options(boomer.ignore = op.boom$boomer.ignore, .local_envir = teardown_env())
